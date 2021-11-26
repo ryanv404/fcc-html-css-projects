b12a3f1d956ad040ef2d11c0817338b97020d687
Ryan <95035038+ryanv404@users.noreply.github.com>
2021-11-26 14:34:49 -0600

    deleted css, js files

--- script.js
+++
@@ -1,8 +0,0 @@
-const dropdown = document.querySelector(".dropdown_content");
-const dropbtn = document.querySelector("#dropbtn");
-const dropicon = document.querySelector(".fa-angle-right");
-
-dropbtn.addEventListener("click", function() {
-  dropdown.classList.toggle("show_drop");
-  dropicon.classList.toggle("rotateArrow");
-});
